44f2a42c0f93df1f7c0687a82b83c1ee481711c5
20taxitaxi00 <takushi0210@icloud.com>
2020-10-27 15:26:05 +0900

    15end

--- script.js
+++ script.js
@@ -38,53 +38,66 @@ for (Q5 of q5){
 
 // Q 6
 let q6 = document.getElementById('q6-text');
-q6.onclick = function(){
-	q6.style.color = "yellow";
-}
+// q6.onclick = function(){
+// 	q6.style.color = "yellow";
+// }
+q6.addEventListener("click",function(){
+	this.style.color = "yellow";
+});
 
 
 // Q 7
 let q7 = document.getElementById('q7-text');
-q7.addEventListener("mouseover",function (event){
-	q7.style.color = "white";
-	q7.style.backgroundColor = "black";
-}
-	)
+q7.addEventListener("mouseover",function(){
+	this.style.color = "white";
+	this.style.backgroundColor = "black";
+});
+
 
 // Q 8
 let q8 = document.getElementById('q8-text');
-q8.addEventListener("mouseover",function (event){
-	q8.style.color = "white";
-	q8.style.backgroundColor = "black";
-}
-	)
-q8.addEventListener("mouseout",function (event){
-	q8.style.color = "black";
-	q8.style.backgroundColor = "white";
-}
-	)
+q8.addEventListener("mouseover",function(){
+	this.style.color = "white";
+	this.style.backgroundColor = "black";
+});
+q8.addEventListener("mouseout",function(){
+	this.style.color = "black";
+	this.style.backgroundColor = "white";
+});
+
 
 
 // Q 9
 let q9name = document.getElementById('q9-input');
 let q9btn = document.getElementById('q9-btn');
 let q9result = document.getElementById('q9-result');
-q9btn.onclick = function(){
+// q9btn.onclick = function(){
+// 	q9result.textContent = q9name.value;
+// }
+q9btn.addEventListener("click",function(){
 	q9result.textContent = q9name.value;
-}
+});
 
 
 // Q 10
 let q10num = document.getElementById('q10-input');
 let q10btn = document.querySelector('.q10-btn');
 let q10result = document.getElementById('q10-result');
-q10btn.onclick = function(){
+// q10btn.onclick = function(){
+// 	if(q10num.value < 20){
+// 		q10result.textContent = "20歳未満の方の利用は禁止です。";
+// 	} else {
+// 		q10result.textContent = q10num.value;
+// 	}
+// }
+q10btn.addEventListener("click",function(){
 	if(q10num.value < 20){
 		q10result.textContent = "20歳未満の方の利用は禁止です。";
 	} else {
 		q10result.textContent = q10num.value;
 	}
-}
+});
+
 
 // Q 11
 let q11name = document.getElementById('q11-input-name');
@@ -92,7 +105,8 @@ let q11age = document.getElementById('q11-input-age');
 let q11btn = document.querySelector('.q11-btn');
 let q11resultname =document.getElementById('q11-result-name');
 let q11resultage = document.getElementById('q11-result-age');
-q11btn.onclick = function(){
+
+q11btn.addEventListener("click",function(){
 	if(!q11name.value && !q11age.value){
 		q11resultname.textContent = "※名前と年齢を入力してください。";
 	} else if(!q11name.value){
@@ -100,27 +114,32 @@ q11btn.onclick = function(){
 		q11resultname.style.color = "red";
 	} else if(!q11age.value){
 		q11resultage.textContent = "※年齢が空欄です。";
-		q11resultage.style.color = "red";
 	} else if(q11age.value < 20){
  		q11resultage.textContent = "20歳未満の方の利用は禁止です。";
 	}　else{
 		q11resultname.textContent = q11name.value;
  		q11resultage.textContent = q11age.value;
 	}
-}
+});
 
 
 // Q 12
 let q12btn = document.querySelector('.q12-btn');
 let q12result = document.querySelector('.q12-result');
-q12btn.onclick = function(){
-	if(!q12result.hasChildNodes()){
-		let p = document.createElement("p");
-		let text = document.createTextNode("Google");
-		p.appendChild(text);
-		q12result.appendChild(p);
-	}
-}
+// q12btn.onclick = function(){
+// 	if(!q12result.hasChildNodes()){
+// 		let p = document.createElement("p");
+// 		let text = document.createTextNode("Google");
+// 		p.appendChild(text);
+// 		q12result.appendChild(p);
+// 	}
+// }
+q12btn.addEventListener("click",function(){
+	let p = document.createElement("p");
+	q12result.appendChild(p);
+	p.textContent = "Google";
+});
+
 
 // Q 13
 // 飛ばしていいです。
@@ -128,10 +147,11 @@ q12btn.onclick = function(){
 
 // Q 14
 let q14btn = document.querySelector('.q14-btn');
-q14btn.onclick = function(){
-	let li = document.getElementById('chicken');
+let li = document.getElementById('chicken');
+q14btn.addEventListener("click",function(){
 	li.remove(li);
-}
+});
+
 
 // Q 15 (難)
 // switchまたはifを使います。
@@ -141,35 +161,54 @@ let q15btn = document.querySelectorAll(".q15-btn");
 let q15link = document.querySelector(".q15-link-list");
 console.log(q15btn);
 console.log(q15btn[0].textContent);
-console.log(q15btn[1].textContent);
-console.log(q15btn[2].textContent);
 
-q15btn.onclick = function(){
-	if(q15btn[0].textContent === 'Google'){
-		let	a =document.createElement("a");
-		let text = document.createTextNode("Google");
-		a.appendChild(text);
-		q15link.appendChild(a);
-		let link = document.querySelector("a");
-		link.href = 'https://google.com';
-	} else if (q15btn[1].textContent === 'YouTube'){
-		let	a =document.createElement("a");
-		let text = document.createTextNode("YouTube");
-		a.appendChild(text);
-		q15link.appendChild(a);
-		let link = document.querySelector("a");
-		link.href = 'https://youtube.com';
-	} else (q15btn[2].textContent === 'Amazon')
-		let	a =document.createElement("a");
-		let text = document.createTextNode("Amazon");
-		a.appendChild(text);
-		q15link.appendChild(a);
-		let link = document.querySelector("a");
-		link.href = 'https://amazon.com';
-}
+// q15btn.addEventListener("click",function(){
+// 	if(q15btn[0].textContent === "Google"){
+// 		var a = document.createElement("a");
+// 		a.textContent = "Google";
+// 		q15link.appendChild(a);
+// 		a.href = 'https://google.com';
+// 	} 
+// 	else if (q15btn[1].textContent == "YouTube"){
+// 		var a = document.createElement("a");
+// 		a.textContent = "YouTube";
+// 		q15link.appendChild(a);
+// 		a.href = 'https://youtube.com';
+// 	} else if(q15btn[2].textContent == "Amazon"){
+// 		var a = document.createElement("a");
+// 		a.textContent = "Amazon";
+// 		q15link.appendChild(a);
+// 		a.href = 'https://amazon.com';
+// 	}
+// });
 
 
 
+
+
+// q15btn.onclick = function(){
+// 	if(q15btn[0].textContent === 'Google'){
+// 		let	a =document.createElement("a");
+// 		let text = document.createTextNode("Google");
+// 		a.appendChild(text);
+// 		q15link.appendChild(a);
+// 		let link = document.querySelector("a");
+// 		link.href = 'https://google.com';
+// 	} else if (q15btn[1].textContent === 'YouTube'){
+// 		let	a =document.createElement("a");
+// 		let text = document.createTextNode("YouTube");
+// 		a.appendChild(text);
+// 		q15link.appendChild(a);
+// 		let link = document.querySelector("a");
+// 		link.href = 'https://youtube.com';
+// 	} else (q15btn[2].textContent === 'Amazon')
+// 		let	a =document.createElement("a");
+// 		let text = document.createTextNode("Amazon");
+// 		a.appendChild(text);
+// 		q15link.appendChild(a);
+// 		let link = document.querySelector("a");
+// 		link.href = 'https://amazon.com';
+// }
 
 
 // switch(q15btn.textContent){
